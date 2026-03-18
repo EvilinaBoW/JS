f2c0bf0f83350fec242542ae29f1b32cbe2edbba
Evilana <pupsik30071982@gmail.com>
2026-03-18 09:28:32 +0300

    Delete 104.js

--- 104.js
+++
@@ -1,6 +0,0 @@
-const curry = function(fn) {
-  return function curried(...args) {
-    if (args.length >= fn.length) return fn(...args);
-    return (...next) => curried(...args, ...next);
-  };
-};
