e3153e22820edea3ef17fc09c63ec84215b32c38
Evilana <pupsik30071982@gmail.com>
2026-03-18 09:35:23 +0300

    Delete 120.js

--- 120.js
+++
@@ -1,7 +0,0 @@
-const createDI = function() {
-  const services = {};
-  return {
-    register: function(name, fn) { services[name] = fn; },
-    get: function(name) { return services[name](); }
-  };
-};
