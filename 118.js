b2383e0bc04f132c03ab7c9daefcf3ada0b0dff3
Evilana <pupsik30071982@gmail.com>
2026-03-18 09:34:51 +0300

    Delete 118.js

--- 118.js
+++
@@ -1,7 +0,0 @@
-const promisify = function(fn) {
-  return function(...args) {
-    return new Promise((resolve, reject) => {
-      fn(...args, (err, result) => err ? reject(err) : resolve(result));
-    });
-  };
-};
