05da9e2955c01e6723950b84d46c03a4b2992768
Evilana <pupsik30071982@gmail.com>
2026-03-18 09:33:53 +0300

    Delete 113.js

--- 113.js
+++
@@ -1,9 +0,0 @@
-const createStateMachine = function(transitions) {
-  let state = Object.keys(transitions)[0];
-  return {
-    getState: function() { return state; },
-    transition: function(event) {
-      state = transitions[state][event] || state;
-    }
-  };
-};
